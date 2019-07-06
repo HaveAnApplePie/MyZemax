dcad524d37561826a29727f500242e975315d286
HaveAnApplePie <50864986+HaveAnApplePie@users.noreply.github.com>
2019-07-06 14:42:29 +0800

    Add files via upload

--- Ray_Point.h
+++ Ray_Point.h
@@ -8,18 +8,19 @@
 
 struct Ray
 {
-	bool	IsInfinite;		//物距是否无穷远
-	int 	surface_number;	//已经过的面
-	int 	Wavelen;		//波长
-	double	L0;				//初始物距
-	double 	U0_D0;			//初始物方半孔径角或入瞳直径
-	double	Y0_W0;			//物高或半视场角   
-	double	Ln;				//当前物距，相对于surface_number
-	double 	Un;				//当前角度
+	bool	IsInfinite;			//物距是否无穷远
+	int 	now_surface_number;	//已经过的面
+	int		all_surface_number;	//总的面数
+	int 	Wavelen;			//波长
+	double	L0;					//初始物距
+	double 	U0_D0;				//初始物方半孔径角或入瞳直径
+	double	Y0_W0;				//物高或半视场角   
+	double	Ln;					//当前物距，相对于surface_number
+	double 	Un;					//当前角度
 };
 
 
-struct Point				//相对于最后一个面与主轴的交点
+struct Point					//相对于最后一个面与主轴的交点
 {
 	double 	X;				
 	double 	Y;				
